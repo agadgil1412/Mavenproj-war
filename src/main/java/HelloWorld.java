74175450d938aa0aa726b700177a1ca13f238c5a
Anagha Gadgil <anaghagadgil@Anaghas-MacBook-Air.local>
2019-08-29 16:36:43 +0530

    proj files

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		System.out.println("This is a sample java progrma for tomcat deployment");
+		System.out.println("Creates a war file using maven and deploy to tomcat using ansible");
+	}
+
+}
